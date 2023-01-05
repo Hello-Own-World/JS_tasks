37d2b27c553ce39335c6d5f3e1a7245131d270e5
Hello-Own-World <yushchakop0188052@gmail.com>
2023-01-05 21:39:06 +0200

    Added docker file and connection to mongodb

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
+if (process.env.NODE_ENV !== "production"){
+    require('dotenv').config()
+}
+
 const express = require("express")
+const mongoose = require("mongoose");
+
 const app = express()
-const expressLayouts = require("express-ejs-layouts")
+// const expressLayouts = require("express-ejs-layouts")
+
+const indexRouter = require("./routes/index")
+
+// mongoose.connect("mongodb://localhost:27017/collectionName", {
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true
+// });
+
+mongoose.connect(process.env.DATABASE_URL, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true
+});
+
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', error => console.log("Connecterd to mongoose"))
 
 app.set("view engine", "ejs")
-app.set("views", __dirname + "/views")
-app.set("layouts", "layouts/layout")
-app.set(expressLayouts)
-app.use(express.static("public"))
+// app.set("views", __dirname + "/views")
+// app.set("layout", "layouts/layout")
+// app.set(expressLayouts)
+// app.use(express.static("public"))
+
+app.use('/', indexRouter)
 
 app.listen(process.env.PORT || 3000)
