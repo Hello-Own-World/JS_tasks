9a9a13fd39dfa6f7f702a8de0e1b0527290d590f
Hello-Own-World <yushchakop0188052@gmail.com>
2023-01-17 00:12:37 +0200

    fix: sophisticated joi validation schemas

--- modules/schemas.js
+++ modules/schemas.js
@@ -4,22 +4,22 @@ const Joi = require('joi')
 const schemas = {
 
   bookPOST: Joi.object().keys({
-    name: Joi.string().required(),
-    author: Joi.string().required(),
-    genre: Joi.string().required()
+    name: Joi.string().max(30).required(),
+    author: Joi.string().min(2).max(30).required(),
+    genre: Joi.string().min(2).max(30).required()
   }),
   sendMsgPOST: Joi.object().keys({
-    body: Joi.string().required()
+    body: Joi.string().max(160).required() // 160 traditional sms message
   }),
   userSignUpPOST: Joi.object().keys({
-    login: Joi.string().required(),
+    login: Joi.string().email().required(),
     pass: Joi.string().required(),
-    firstName: Joi.string().required(),
-    lastName: Joi.string().required(),
-    phone: Joi.string().required()
+    firstName: Joi.string().required(), //better avoid validation
+    lastName: Joi.string().required(),  //better avoid validation
+    phone: Joi.number().integer().positive().required()
   }),
   UserSignInPOST: Joi.object().keys({
-    login: Joi.string().required(),
+    login: Joi.string().email().required(),
     pass: Joi.string().required()
   })
 
