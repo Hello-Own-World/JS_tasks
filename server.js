1ab9e1612d67eee338db106c2a1a14896a78fce3
Hello-Own-World <yushchakop0188052@gmail.com>
2023-02-01 02:46:21 +0200

    fix: allow CORS for browser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
 
 const { initConnection } = require('./config/db');
 const defaultQueue = require('./queue');
@@ -20,6 +21,8 @@ app.set('views', path.join(__dirname, '/views'));
 app.set('layout', 'layouts/layout');
 
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
   req.defaultQueue = defaultQueue;
